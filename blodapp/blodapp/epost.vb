d4bd56d584746754d11b725121ec65890bf3d5ed
crispocolus <kristoffer.selbekk.hille@gmail.com>
2017-03-31 11:21:52 +0200

    mega commit
    yeas

---
+++ blodapp/blodapp/epost.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Net.Mail
+Public Class epost
+    Public Sub sendEpost(message As String, mottaker As String)
+        Try
+            Dim Smtp_Server As New SmtpClient
+            Dim e_mail As New MailMessage()
+            Smtp_Server.UseDefaultCredentials = False
+            Smtp_Server.Credentials = New Net.NetworkCredential("bloodflowas@gmail.com", "bloodwillflow")
+            Smtp_Server.Port = 587
+            Smtp_Server.EnableSsl = True
+            Smtp_Server.Host = "smtp.gmail.com"
+
+            e_mail = New MailMessage()
+            e_mail.From = New MailAddress("bloodflowas@gmail.com")
+            e_mail.To.Add(mottaker)
+            e_mail.Subject = "Email Sending"
+            e_mail.IsBodyHtml = False
+            e_mail.Body = message
+            Smtp_Server.Send(e_mail)
+            MsgBox("Mail Sent")
+
+        Catch error_t As Exception
+            MsgBox(error_t.ToString)
+        End Try
+
+    End Sub
+End Class
