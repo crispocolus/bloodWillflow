fe5a05d79744b2af734e4eb16c6ec68e00c3c199
OleValla <olevalla.wow@gmail.com>
2017-04-21 11:59:18 +0200

    Minifix

--- blodapp/blodapp/adminSide.vb
+++ blodapp/blodapp/adminSide.vb
@@ -38,7 +38,7 @@
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim reg As New RegistrerForm
-        reg.SjekkInfo(postnrTxt.Text, adresseTxt.Text, pnummerTxt.Text, fornavnTxt.Text, etternavnTxt.Text, epostTxt.Text, tlfTxt.Text, RegistrerForm.finnFdato(pnummerTxt.Text), passordcTxt.Text, passordTxt.Text)
+        reg.SjekkInfo(postnrTxt.Text, adresseTxt.Text, pnummerTxt.Text, fornavnTxt.Text, etternavnTxt.Text, epostTxt.Text, tlfTxt.Text, passordcTxt.Text, passordTxt.Text)
 
     End Sub
 
